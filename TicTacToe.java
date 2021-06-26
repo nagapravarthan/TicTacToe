2360430e1006ef2e71a923151f7634009435759f
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-26 21:32:13 +0530

    TicTacToeuc1

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,6 +2,21 @@ class TicTacToe
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to TicTacToe Game");
-}
+	System.out.println("Welcome to TicTacToe Game");
+	Board();
     }
+
+    public static void Board()
+    {
+    char[] board=new char[8];
+    for(int i=0;i<=2;i++)
+     {
+        for(int j=0;j<=2;j++)
+         {
+            System.out.print('_');
+            System.out.print('|');
+         }
+        System.out.println();
+     }
+    }
+}
